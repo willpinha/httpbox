2b329573bfb3887151f382db43d43ec755ef0272
Willian Pinheiro <willpinhaaa@gmail.com>
2025-10-23 20:56:20 -0300

    Improve error handling by passing the original error to WithDetails in JSON, XML, and byte reading functions

--- request.go
+++ request.go
@@ -24,7 +24,7 @@ func ReadJSON[T any](r io.Reader) (T, error) {
 	var v T
 
 	if err := json.NewDecoder(r).Decode(&v); err != nil {
-		return v, NewError(http.StatusBadRequest, "invalid JSON body", WithDetails(err.Error()))
+		return v, NewError(http.StatusBadRequest, "invalid JSON body", WithDetails(err))
 	}
 
 	if err := verifyValidator(v); err != nil {
@@ -38,7 +38,7 @@ func ReadXML[T any](r io.Reader) (T, error) {
 	var v T
 
 	if err := xml.NewDecoder(r).Decode(&v); err != nil {
-		return v, NewError(http.StatusBadRequest, "invalid XML body", WithDetails(err.Error()))
+		return v, NewError(http.StatusBadRequest, "invalid XML body", WithDetails(err))
 	}
 
 	if err := verifyValidator(v); err != nil {
@@ -51,7 +51,7 @@ func ReadXML[T any](r io.Reader) (T, error) {
 func ReadBytes(r io.Reader) ([]byte, error) {
 	data, err := io.ReadAll(r)
 	if err != nil {
-		return nil, NewError(http.StatusBadRequest, "unable to read body", WithDetails(err.Error()))
+		return nil, NewError(http.StatusBadRequest, "unable to read body", WithDetails(err))
 	}
 
 	return data, nil
