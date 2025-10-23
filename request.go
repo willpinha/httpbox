3f80fa04c358dec85bd0109bc3587c419f69745c
Willian Pinheiro <willpinhaaa@gmail.com>
2025-10-23 20:53:49 -0300

    Add request handling functions for JSON, XML, and byte reading

---
+++ request.go
@@ -0,0 +1,58 @@
+package httpbox
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"net/http"
+)
+
+type Validator interface {
+	Validate() error
+}
+
+func verifyValidator(v any) error {
+	validator, ok := v.(Validator)
+	if !ok {
+		return nil
+	}
+
+	return validator.Validate()
+}
+
+func ReadJSON[T any](r io.Reader) (T, error) {
+	var v T
+
+	if err := json.NewDecoder(r).Decode(&v); err != nil {
+		return v, NewError(http.StatusBadRequest, "invalid JSON body", WithDetails(err.Error()))
+	}
+
+	if err := verifyValidator(v); err != nil {
+		return v, err
+	}
+
+	return v, nil
+}
+
+func ReadXML[T any](r io.Reader) (T, error) {
+	var v T
+
+	if err := xml.NewDecoder(r).Decode(&v); err != nil {
+		return v, NewError(http.StatusBadRequest, "invalid XML body", WithDetails(err.Error()))
+	}
+
+	if err := verifyValidator(v); err != nil {
+		return v, err
+	}
+
+	return v, nil
+}
+
+func ReadBytes(r io.Reader) ([]byte, error) {
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return nil, NewError(http.StatusBadRequest, "unable to read body", WithDetails(err.Error()))
+	}
+
+	return data, nil
+}
